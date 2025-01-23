43095f5b577a7f1b019e373e52754c5123316f0f
shresth-kumar-lal <112205812+shresth-kumar-lal@users.noreply.github.com>
2025-01-24 04:50:38 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,34 @@
+'use strict';
+
+require('dotenv').config();
+
+var express = require('express');
+var cors = require('cors');
+
+var multer = require('multer');
+var upload = multer({ dest: 'uploads/' });
+
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  try {
+    res.json({
+      "name": req.file.originalname,
+      "type": req.file.mimetype,
+      "size": req.file.size
+    });
+  } catch (err) {
+    res.send(400);
+  }
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Running on port 3000');
+});
